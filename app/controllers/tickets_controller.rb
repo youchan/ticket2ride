7dbf0a4adfaa92ce4f937a1ca6660df2859f7a7b
youchan <youchan01@gmail.com>
2013-02-27 16:19:12 +0900

    bootstrap に対応

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 class TicketsController < ApplicationController
+
     def index
         @tickets = Ticket.all(:order => "created_at DESC")
     end
@@ -11,25 +12,34 @@ class TicketsController < ApplicationController
 
     def new
         @ticket = Ticket.new
+        @extended_fields = [ { :name => :test_field }]
     end
 
     def create
         @ticket = Ticket.new(params[:ticket])
         if @ticket.save
-            redirect_to tickets_path, notice: '新しいチケットが作成されました！'
+            redirect_to tickets_path, notice: "新しいチケット『" + @ticket.title + "』が作成されました！"
         else
-            render action: 'new'
+            render action: "new"
         end
     end
 
     def edit
         @ticket = Ticket.find(params[:id])
+        @extended_fields = [ { :name => :test_field }]
     end
 
     def update
         @ticket = Ticket.find(params[:id])
         if @ticket.update_attributes(params[:ticket])
-            redirect_to tickets_path, notice: '更新されました！'
+            redirect_to tickets_path, notice: "『" + @ticket.title + "』が更新されました！"
         end
     end
+
+    def destroy
+        @ticket = Ticket.find(params[:id])
+        @ticket.destroy
+        redirect_to tickets_path, notice: "『" + @ticket.title + "』が削除されました！"
+    end
+
 end
